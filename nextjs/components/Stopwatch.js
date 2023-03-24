cae42707458ec47745570305a237bc89f6859761
zhaoqix2 <zhaoqix2@illinois.edu>
2023-03-24 17:48:18 -0500

    Revised the stopwatch functions and add some styles to the buttons

--- nextjs/components/Stopwatch.js
+++ nextjs/components/Stopwatch.js
@@ -1,32 +1,79 @@
-import { useState, useRef } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import style from "./Stopwatch.module.css";
 import Button from 'react-bootstrap/Button';
+// export default function Stopwatch(props) {
+//   const [time, setTime] = useState(0);
+//   const [now, setNow] = useState(0);
+  
+//   const intervalRef = useRef(0);
+
+//   const handleStart = () => {
+//     setTime(Date.now());
+//     setNow(Date.now());
+//     intervalRef.current = setInterval(() => {
+//       setTime(Date.now());
+//     }, 10);
+//   };
+  
+//   const handleStop = () => {
+//     clearInterval(intervalRef.current);
+//   };
+
+//   let timePassed = Math.trunc((time - now) / 1000);
+
+//   let timePassed_min = Math.trunc(timePassed / 60).toLocaleString('en-US', {
+//     minimumIntegerDigits: 2,
+//     useGrouping: false
+//   });
+  
+//   let timePassed_sec = (timePassed - 60*timePassed_min).toLocaleString('en-US', {
+//     minimumIntegerDigits: 2,
+//     useGrouping: false
+//   });
+
+//   return (
+//     <div className='Stopwatch'>
+//       <h1 className={style.title}>Start recording time</h1>
+//       <h2>{timePassed_min}:{timePassed_sec}</h2>
+//       <div>
+//         <Button variant="success" onClick={handleStart}>Start</Button>
+//         <Button variant="warning" onClick={handleStop}>Stop</Button>
+//       </div>
+//     </div>
+//   )
+// }
+
 export default function Stopwatch(props) {
   const [time, setTime] = useState(0);
-  const [now, setNow] = useState(0);
-  
-  const intervalRef = useRef(0);
+  const [isActive, setIsActive] = useState(false);
 
-  const handleStart = () => {
-    setTime(Date.now());
-    setNow(Date.now());
-    intervalRef.current = setInterval(() => {
-      setTime(Date.now());
-    }, 10);
+  useEffect(() => {
+    let interval = null;
+    if (isActive) {
+      interval = setInterval(() => {
+        setTime(time => time + 1);
+      }, 1000);
+    } else {
+      clearInterval(interval);
+    }
+    return () => clearInterval(interval);
+  }, [isActive, time]);
+
+  const handleStartPause = () => {
+    setIsActive(!isActive);
   };
   
-  const handleStop = () => {
-    clearInterval(intervalRef.current);
+  const handleReset = () => {
+    setTime(0);
+    setIsActive(false);
   };
 
-  let timePassed = Math.trunc((time - now) / 1000);
-
-  let timePassed_min = Math.trunc(timePassed / 60).toLocaleString('en-US', {
+  let min = Math.trunc(time / 60).toLocaleString('en-US', {
     minimumIntegerDigits: 2,
     useGrouping: false
   });
   
-  let timePassed_sec = (timePassed - 60*timePassed_min).toLocaleString('en-US', {
+  let sec = (time - 60*min).toLocaleString('en-US', {
     minimumIntegerDigits: 2,
     useGrouping: false
   });
@@ -34,10 +81,10 @@ export default function Stopwatch(props) {
   return (
     <div className='Stopwatch'>
       <h1 className={style.title}>Start recording time</h1>
-      <h2>{timePassed_min}:{timePassed_sec}</h2>
+      <h2>{min}:{sec}</h2>
       <div>
-        <Button variant="success" onClick={handleStart}>Start</Button>
-        <Button variant="warning" onClick={handleStop}>Stop</Button>
+        <Button variant="success" onClick={handleStartPause} className={style.button}>{isActive ? 'Pause' : 'Start'}</Button>
+        <Button variant="warning" onClick={handleReset} className={style.button}>Reset</Button>
       </div>
     </div>
   )
