411e7cf4f5176836ae947dce84e5a51e9c5b2ca4
Qiuchi-Li <qiuchil2@illinois.edu>
2023-03-26 00:57:35 -0500

    Added login button on the navigation bar and link it to the login form. Still implementing the login form.

---
+++ nextjs/pages/login.js
@@ -0,0 +1,15 @@
+
+
+export default function LoginPage() {
+    function addLoginHandler(loginData) {
+  
+    }
+    
+    return (
+      <div>
+        <h1>Login</h1>
+        {/* can pass function in props as well! */}
+        {/* <NewMeetupForm onAddMeetup={addMeetupHandler}/> */}
+      </div>
+    )
+  }
